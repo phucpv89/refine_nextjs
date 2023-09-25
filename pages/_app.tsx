5dbefcf8942ce654b63c96ecff6afe5d8dc3b9ae
phucpv <phucpv@vng.com.vn>
2023-09-25 14:11:19 +0700

    Initial commit from create-refine-app

---
+++ pages/_app.tsx
@@ -0,0 +1,96 @@
+import {
+  ThemedLayoutV2,
+  ThemedSiderV2,
+  notificationProvider,
+} from "@refinedev/antd";
+import { GitHubBanner, Refine } from "@refinedev/core";
+import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
+import routerProvider, {
+  DocumentTitleHandler,
+  UnsavedChangesNotifier,
+} from "@refinedev/nextjs-router";
+import type { NextPage } from "next";
+import { AppProps } from "next/app";
+
+import { Header } from "@components/header";
+import { ColorModeContextProvider } from "@contexts";
+import "@refinedev/antd/dist/reset.css";
+import dataProvider from "@refinedev/simple-rest";
+import { authProvider } from "src/authProvider";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  noLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
+  const renderComponent = () => {
+    if (Component.noLayout) {
+      return <Component {...pageProps} />;
+    }
+
+    return (
+      <ThemedLayoutV2
+        Header={() => <Header sticky />}
+        Sider={(props) => <ThemedSiderV2 {...props} fixed />}
+      >
+        <Component {...pageProps} />
+      </ThemedLayoutV2>
+    );
+  };
+
+  return (
+    <>
+      <GitHubBanner />
+      <RefineKbarProvider>
+        <ColorModeContextProvider>
+          <Refine
+            routerProvider={routerProvider}
+            dataProvider={dataProvider(API_URL)}
+            notificationProvider={notificationProvider}
+            authProvider={authProvider}
+            resources={[
+              {
+                name: "blog_posts",
+                list: "/blog-posts",
+                create: "/blog-posts/create",
+                edit: "/blog-posts/edit/:id",
+                show: "/blog-posts/show/:id",
+                meta: {
+                  canDelete: true,
+                },
+              },
+              {
+                name: "categories",
+                list: "/categories",
+                create: "/categories/create",
+                edit: "/categories/edit/:id",
+                show: "/categories/show/:id",
+                meta: {
+                  canDelete: true,
+                },
+              },
+            ]}
+            options={{
+              syncWithLocation: true,
+              warnWhenUnsavedChanges: true,
+              projectId: "o6kPX8-T9nKSv-IA1YWk",
+            }}
+          >
+            {renderComponent()}
+            <RefineKbar />
+            <UnsavedChangesNotifier />
+            <DocumentTitleHandler />
+          </Refine>
+        </ColorModeContextProvider>
+      </RefineKbarProvider>
+    </>
+  );
+}
+
+export default MyApp;
